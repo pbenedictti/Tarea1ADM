b1d10caa136688df2b28e38c5c6eff7e6c57591b
bfariello <bfariellosierra@gmail.com>
2026-04-21 21:04:13 -0300

    primer subida
    primer subida

---
+++ app/src/main/java/com/adm/adm_0605/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.adm.adm_0605
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.adm.adm_0605.ui.theme.ADM_0605Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ADM_0605Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color(0xFFB710E7)
+                ) {
+                    Greeting(name = "ADM")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.home),
+            contentDescription = "Logo de la app",
+            modifier = Modifier.size(300.dp),
+            contentScale = ContentScale.Crop 
+        )
+        
+        Text(
+            text = "¡Bienvenido a $name!",
+            fontSize = 24.sp,
+            color = Color.White,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ADM_0605Theme {
+        Surface(color = Color(0xFFB710E7)) {
+            Greeting("ADM")
+        }
+    }
+}

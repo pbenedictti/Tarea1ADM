0bcd5b8af276c7d0b0467d8ed32ff9dbd209eb72
pbenedictti <60271941+pbenedictti@users.noreply.github.com>
2026-05-12 21:29:03 -0300

    version demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.adm.adm_0605"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
